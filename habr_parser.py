c60d473ced43fffc4b822f5c541ae3b6342d2606
Kirill Tablesky <lightmap.tabelskiy@gmail.com>
2022-11-24 11:59:55 +0200

    initial

---
+++ habr_parser.py
@@ -0,0 +1,46 @@
+import requests
+from fake_headers import Headers
+from bs4 import BeautifulSoup
+
+HOST = 'https://habr.com'
+ARTICLES = f'{HOST}/ru/all/'
+
+
+def get_heders():
+    return Headers(browser='firefox', os='win').generate()
+
+
+def get_text(url):
+    return requests.get(url, headers=get_heders()).text
+
+
+def parse_article(article_tag):
+    link_tag = article_tag.find('a', class_="tm-article-snippet__title-link")
+    if link_tag is None:
+        return
+    link = link_tag['href']
+    link = f'{HOST}{link}'
+    return {
+        'date': article_tag.find('time')['title'],
+        'link': link,
+        'title': link_tag.find('span').text,
+    }
+
+
+def parse_page(page):
+    html = get_text(f'{ARTICLES}page{page}')
+    soup = BeautifulSoup(html, features='html5lib')
+    articles = soup.find(class_="tm-articles-list").find_all('article')
+    articles_parsed = []
+    for article in articles:
+        parsed = parse_article(article)
+        articles_parsed.append(parsed)
+    return articles_parsed
+
+
+if __name__ == '__main__':
+    for page in range(1, 51):
+        print(page)
+        print(parse_page(page))
+
+
---
+++ my_ip_regex.py
@@ -0,0 +1,15 @@
+import requests
+import re
+
+HOST = 'https://2ip.ru/'
+
+IP_REGEX = re.compile(r'<div.*?id="d_clip_button".*>\s*<span>(.*?)</span>')
+
+
+def get_ip():
+    html = requests.get(HOST).text
+    return IP_REGEX.search(html).group(1)
+
+
+if __name__ == '__main__':
+    print(get_ip())
